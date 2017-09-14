754e827153dc01518d6e76946bf7d03d550c6950
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-09-14 13:37:05 +0300

    gradle kotlin plugin

--- app/src/main/java/com/kc/pr/nagy/mohamed/keyconnector/network/WifiManagerClass.java
+++ app/src/main/java/com/kc/pr/nagy/mohamed/keyconnector/network/WifiManagerClass.java
@@ -1,20 +1,15 @@
-package com.kc.pr.nagy.mohamed.keyconnector.network;
+package com.kc.pr.nagy.mohamed.keyconnector.network
 
 /**
  * Created by mohamednagy on 8/24/2017.
  */
 
 
-public enum WifiManagerClass {
+enum class WifiManagerClass private constructor(var stringMethodName: String) {
     getWifiApConfiguration("getWifiApConfiguration"),
     setWifiApConfiguration("setWifiApConfiguration"),
     isWifiEnabled("isWifiEnabled"),
     setWifiApEnabled("setWifiApEnabled"),
     isWifiApEnabled("isWifiApEnabled"),
-    setWifiEnabled("setWifiEnabled");
-
-    public String stringMethodName;
-    WifiManagerClass(String stringName){
-        stringMethodName = stringName;
-    }
+    setWifiEnabled("setWifiEnabled")
 }
